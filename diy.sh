8413d3ff39424afcd374508f74bfc893b8cb8fa7
yyleis <60906301+yyleis@users.noreply.github.com>
2025-02-11 21:31:58 +0800

    Update diy.sh
    Signed-off-by: yyleis <60906301+yyleis@users.noreply.github.com>

--- diy.sh
+++ diy.sh
@@ -3,5 +3,5 @@ rm -rf luci-app-msd_lite
 git clone --recursive  https://github.com/hejiadong0608/luci-app-msd_lite.git
 
 rm -rf luci-app-unblockneteasemusic
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+#git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
 cd ../../
